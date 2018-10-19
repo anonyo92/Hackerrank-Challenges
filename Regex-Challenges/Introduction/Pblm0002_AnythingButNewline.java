46f348507dbae5f6f9d5543c4905fcbfbc0781aa
Ananya Palit <plt.anonX@gmail.com>
2018-10-19 07:15:39 -0400

    Added problem description

--- Regex-Challenges/Introduction/Pblm0002_AnythingButNewline.java
+++ Regex-Challenges/Introduction/Pblm0002_AnythingButNewline.java
@@ -1,3 +1,6 @@
+/** Match only and exactly strings of form: abc.def.ghi.jkl , 
+ * where each variable a,b,c,d,e,f,g,h,i,j,k,l can be any single character except the newline. 
+ */
 public class Solution
 {
 	public static void main(String[] args) {
