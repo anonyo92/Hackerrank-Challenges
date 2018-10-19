a8420e5c10e98b0a5af3f0a636152d819c276d37
Ananya Palit <plt.anonX@gmail.com>
2018-10-19 08:55:01 -0400

    Update Regex-Challenges/Introduction/Pblm0001_MatchStr.java

--- Regex-Challenges/Introduction/Pblm0001_MatchStr.java
+++
@@ -1,8 +0,0 @@
-/* Match the String 'hackerrank' */
-public class Solution 
-{
-	public static void main(String[] args) {
-		Regex_Test tester = new Regex_Test();
-		tester.checker("hackerrank");
-	}
-}
